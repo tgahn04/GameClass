995ff66ff6c6ffad936459b1171c6e080678554a
tgahn04 <tgahn0524@gmail.com>
2025-07-17 18:55:16 +0900

    임시

--- Program/Source1.c
+++ Program/Source1.c
@@ -1,76 +1,48 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 void main()
 {
-#pragma region 변수
-	// 데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
-
-#pragma region 자료형
-	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
-
-	// 자료형은 각각의 자료형마다 크기가 정해져 있으며, 자료형의
-	// 크기는 바이트 단위로 이루어져 있고, 자료형에 따라 저장할
-	// 수 있는 값의 종류와 범위가 결정됩니다.
+#pragma region 비트
+	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의 조합으로
+	// 논리 계산을 수행하는 단위입니다.
 #pragma endregion
 
-#pragma region 서식 지정자
-	// 변수의 값을 출력하기 위한 자료형의 정보를 명시적으로
-	// 지정해주는 것입니다.
+#pragma region (10)진수를 2진수로 변환하는 과정
+	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
+	// 나는 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
 
-	// 서식 지정자의 경우 하나의 표준 출력 함수에 여러 개의
-	// 서식을 넣을 수 있으며, 서식 지정자의 순서는 왼쪽에서
-	// 시작합니다.
-
-	// 서식 지정자는 각각의 서식에 따라 원하는 데이터를 출력할
-	// 수 있으며. 서식과 변수의 형태가 일치하지 않으면 원하는
-	// 값이 출력되지 않습니다.
-
-	//char alphabet = 'A';
-	// %c - char 
-
-	//int integer = 100;
-	// %d - int
-
-	//float decimal = 36.5f;
-	// %f - float
-
-	//printf("alphabet = %c\n", alphabet);
-	//printf("integer = %d\n", integer);
-	//printf("decimal = %f\n", decimal);
-
-#pragma endregion
-
-#pragma region 변수의 이름 규칙
-
-	// 1. 변수의 이름은 대소문자를 구분합니다.
-	// ex) int health = 100;
-	// ex) int HEALTH = 20;
-	
-	// 2. 변수의 이름으로 예약어를 사용할 수 없습니다.
-	// ex) int int;
-
-	// 3. 변수의 이름으로 공백이 포함될 수 없습니다.
-	// ex) float time Delta;
-
-	// 4. 변수의 이름으로 특수 문자는 "_", "$"만 사용할 수 있습니다.
-	// ex)int under_Score;
-	// ex)int currentRoom$;
-
-	// 5. 변수의 이름은 숫자로 시작할 수 없습니다.
-	// ex) int 5error;
-	// ex) int error2022;
+	// 2 23 
+	// 2 11 1
+	// 2  5 1
+	// 2  2 1
+	//    1 0  =  10111
 #pragma endregion
 
 
-	int experience = 100;
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1 byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
 
-	experience = 200;
+	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
+	// 다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
 
-	printf("experience : %d\n", experience);
 
-	// 변수는 프로그램이 실행되는 동안 값을 바꿀 수 있으며,
-	// 원래 저장되어 있는 값은 새로 저장되는 값에 의해 지워집니다.
 #pragma endregion
+
+#pragma region 비트 연산자
+
+	// 산술 연산자 +, -, *, /, %     비트 연산자 AND - &
+	//                                           OR - |
+	//                                           XOR - ^
+	//                                           NOT - ~
+
+
+#pragma endregion
+
+
+
+
+#pragma endregion
+
 
 
 }
