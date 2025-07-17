643f2aa94b0ec63a23e0142d2b337fc2b397f332
tgahn04 <tgahn0524@gmail.com>
2025-07-17 19:46:19 +0900

    (2025.7.17) 비트

--- Program/Source1.c
+++ Program/Source1.c
@@ -2,12 +2,30 @@
 
 void main()
 {
+
+#pragma region 산술 연산자
+
+	// 1, 변수 = 변수 + 변수
+
+	// 2. 변수 = 변수 - 리터럴 상수
+
+	// 3. 변수 = 변수 * 심볼릭 상수
+
+	// 4. 변수 = 리터럴 상수 / 리터럴 상수
+
+	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+
+
+#pragma endregion
+
+
+
+
 #pragma region 비트
 	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의 조합으로
 	// 논리 계산을 수행하는 단위입니다.
-#pragma endregion
 
-#pragma region (10)진수를 2진수로 변환하는 과정
+#pragma region (10)진수를 (2)진수로 변환하는 과정
 	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
 	// 나는 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
 
@@ -29,11 +47,53 @@ void main()
 #pragma endregion
 
 #pragma region 비트 연산자
+	//비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
 
-	// 산술 연산자 +, -, *, /, %     비트 연산자 AND - &
-	//                                           OR - |
-	//                                           XOR - ^
-	//                                           NOT - ~
+#pragma region AND 연산자
+	// 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자입니다.
+
+	//int storage1 = 10; //0000 1010
+	//int storage2 = 6;  //0000 0110
+	                   //--------
+	                   //0000 0010
+	
+	//printf("storage1 & storage2 = %d\n", storage1 & storage2);
+
+#pragma endregion
+
+#pragma region OR 연산자
+	// 두 개의 피연산자 중에 하나라도 1이 있다면 1을 반환하는 연산자입니다.
+	
+	//int storage3 = 8;
+	//int storage4 = 12;
+
+	//printf("storage3 | storage4 = %d\n", storage3 | storage4);
+
+
+
+#pragma endregion
+
+#pragma region XOR 연산자
+	// 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면
+	// 1을 반환하는 연산자입니다.
+
+	//int storage5 = 6;
+	//int storage6 = 15;
+
+	//printf("storage5 ^ storage6 = %d\n", storage5 ^ storage6);
+
+
+#pragma endregion
+
+#pragma region NOT 연산자
+	// 하나의 비트를 반전시키는 연산자입니다.
+
+	//int storage7 = 13;
+
+	//	printf("~storage7 = %d\n", ~storage7);
+
+		// 첫 번째 비트는 부호를 나타내며, 첫 번째 비트에
+		// 1이 있다면 값은 음수입니다. ex) 1111 1101 = 음수
 
 
 #pragma endregion
@@ -43,6 +103,6 @@ void main()
 
 #pragma endregion
 
-
+#pragma endregion
 
 }
