9ccb45eb818edcd0c667c4ef78c5b79d2882ffef
tgahn04 <tgahn0524@gmail.com>
2025-07-29 19:45:47 +0900

    (2025.7.29) 배열

---
+++ Class8th (Pointer)/Source.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자입니다.
+
+	// int data = 10;
+
+	// printf("변수의 주소 = %p\n", &data);
+
+	// 데이터의 주소는 해당 데이터가 저장된 메모리의 시작 주소를
+	// 의미하며, 메모리 공간은 1 byte의 크기로 나누어 표현합니다.
+#pragma endregion
+
+#pragma region scanf_s 함수
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에
+	// 맞추어 입력해주는 함수입니다.
+
+	// int x = 0;
+
+	// printf("x 변수의 값을 입력해주세요.\n");
+
+	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로
+	// 넘어갈 수 없습니다.
+
+	// scanf_s("%d", &x);
+
+	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며,
+	// 서식 지정자에 따라 입력할 수 있는 데이터의 범위도 결정됩니다.
+
+	// printf("x 변수의 값 = %d\n", x);
+
+	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
+	// 데이터를 보관하였다가 입력하는 순간 버퍼 안의
+	// 내용을 프로그램에 전송합니다.
+#pragma endregion
+
+#pragma region 포인터
+	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
+
+	// int storage = 10;
+
+	// int * pointer = &storage;
+
+	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
+	// 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
+
+	// printf("storage 변수의 주소 = %p\n", &storage);
+
+	// printf("pointer의 값 = %p\n", pointer);
+
+	// printf("pointer 변수의 주소 = %p\n\n", &pointer);
+
+	// *pointer = 99;
+
+	// printf("storage 변수의 값 = %d\n", storage);
+
+	// printf("pointer 변수의 값 = %d\n\n", *pointer);
+
+	// int variable = 20;
+
+	// pointer = &variable;
+
+	// printf("variable 변수의 주소 = %p\n", &variable);
+
+	// printf("pointer 변수의 주소 = %p\n", &pointer);
+
+	// *pointer = 100;
+
+	// printf("variable 변수의 값 = %d\n", variable);
+
+	// printf("pointer 변수의 값 = %d\n", *pointer);
+
+	// printf("pointer 변수가 가리키는 값 = %d\n\n", *pointer);
+
+	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
+	// 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의
+	// 시작 주소를 가리키게 됩니다.
+
+	// printf("int 크기 = %u\n", sizeof(int));
+
+	// printf("pointer 변수의크기 = %u\n", sizeof(pointer));
+
+	//포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수
+	// 있는 크기로 정해지며, 한 번에 처리할 수 있는 크기는
+	// 운영체제에 따라 크기가 결정됩니다.
+#pragma endregion
+
+#pragma region 숙제
+
+	// printf("간단 계산기\n\n");
+	// 
+	// double num1, num2, value;
+	// char operator;
+	// 
+	// printf("첫번째 숫자 = ");
+	// scanf_s("%lf", &num1);
+	// 
+	// printf("연산자 = ");
+	// scanf_s(" %c", &operator, 1);
+	// 
+	// printf("두번째 숫자 = ");
+	// scanf_s("%lf", &num2);
+	// 
+	// value = num1;
+	// 
+	// switch (operator)
+	// {
+	// case '+' : 
+	// 	value += num2;
+	// 		printf("%.2lf + %.2lf = %.2lf\n", num1, num2, value);
+	// 		break;
+	// 
+	// case '-' :
+	// 	value -= num2;
+	// 		printf("%.2lf - %.2lf = %.2lf", num1, num2, value);
+	// 		break;
+	// case '*':
+	// 	value *= num2;
+	// 	printf("%.2lf * %.2lf = %.2lf", num1, num2, value);
+	// 	break;
+	// 
+	// case '/':
+	// 	
+	// 	value /= num2;
+	// 	printf("%.2lf / %.2lf = %.2lf", num1, num2, value);
+	// 	break;
+	// 
+	// default:
+	// 	printf("오류\n");
+	// 	break;
+	// }
+	// return 0;
+
+#pragma endregion
+
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을
+	// 저장할 수 있는 포인터입니다.
+
+	// char rank = 'A';
+	// int count = 10;
+	// float angle = 3.14f;
+
+	// void* reference = NULL; // 포인터 초기화
+
+	// reference = &rank;
+
+	// 범용 포인터는 메모리 주소에 접근해서 값을
+	// 변경할 수 없습니다.
+
+	// *(char *)reference = 'B';
+
+	// printf("rank 변수의 값 = %c\n", rank);
+	// printf("reference 변수의 값 = %p\n\n", reference);
+
+	// reference = &count;
+
+	// *(int*)reference = 50;
+
+	// printf("count 변수의 값 = %d\n", count);
+	// printf("reference 변수의 값 = %p\n\n", reference);
+
+	// reference = &angle;
+
+	// *(float*)reference = 45.5f;
+	// printf("angle 변수의 값 = %f\n", angle);
+	// printf("reference 변수의 값 = %p\n\n", reference);
+
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
+	// 가리키는 변수의 자료형으로 형 변환 해주어야 합니다.
+#pragma endregion
+
+#pragma region 상수 지시 포인터
+// 포인터가 가리키는 주소에 저장되어 있는 값을 변경할 수 없도록
+// 지정되어 있는 포인터 입니다.
+
+// int room1 = 10;
+// int room2 = 20;
+
+// const int * home = NULL;
+
+// home = &room1;
+
+// *home = 99; (x)
+
+// printf("room1의 주소 값 = %p\n", &room1);
+// printf("home의 값 = %p\n\n", home);
+
+// home = &room2;
+
+// printf("room2의 주소 값 = %p\n", &room2);
+// printf("home의 값 = %p\n", home);
+#pragma endregion
+
+#pragma region 포인터 상수
+// 포인터 변수가 가리키고 있는 주소 값을
+// 변경할 수 없는 포인터 입니다.
+
+ // int room1 = 99;
+ // int room2 = 555;
+
+ // int* const fixed_pointer = &room1;
+
+ // *fixed_pointer = 158;
+
+ // printf("fixed_pointer 변수가 가리키는 값 = %d\n", *fixed_pointer);
+ // printf("room1 변수의 값 = %d\n", room1);
+
+ // fixed_pointer = &room2;
+
+ // 상수로 선언한 포인터는 해당 변수의 값을 변경할 수 있지만, 다른 변수의
+ // 주소를 새로 저장할 수는 없습니다.
+#pragma endregion
+}
--- Program/Source1.c
+++ Program/Source1.c
@@ -2,217 +2,51 @@
 
 void main()
 {
-#pragma region 주소 연산자
-	// 변수의 주소 값을 반환하는 연산자입니다.
-
-	// int data = 10;
-
-	// printf("변수의 주소 = %p\n", &data);
-
-	// 데이터의 주소는 해당 데이터가 저장된 메모리의 시작 주소를
-	// 의미하며, 메모리 공간은 1 byte의 크기로 나누어 표현합니다.
-#pragma endregion
-
-#pragma region scanf_s 함수
-	// 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에
-	// 맞추어 입력해주는 함수입니다.
-
-	// int x = 0;
-
-	// printf("x 변수의 값을 입력해주세요.\n");
-
-	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로
-	// 넘어갈 수 없습니다.
-
-	// scanf_s("%d", &x);
-
-	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며,
-	// 서식 지정자에 따라 입력할 수 있는 데이터의 범위도 결정됩니다.
-
-	// printf("x 변수의 값 = %d\n", x);
-
-	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
-	// 데이터를 보관하였다가 입력하는 순간 버퍼 안의
-	// 내용을 프로그램에 전송합니다.
-#pragma endregion
-
-#pragma region 포인터
-	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
-
-	// int storage = 10;
-
-	// int * pointer = &storage;
-
-	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
-	// 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
-
-	// printf("storage 변수의 주소 = %p\n", &storage);
+#pragma region 배열
+	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+	
+	 // int list[5];
+	
+	 // 배열의 경우 첫 번째 원소는 0부터 시작합니다.
+	
+	 // for (int i = 0; i < 5; i++)
+	 // {
+	 //		list[i] = (i + 1) * 10;
 	 
-	// printf("pointer의 값 = %p\n", pointer);
+	 // 	printf("list[%d]의 값 = %d\n", i, list[i]);
+	 // }
 	
-	// printf("pointer 변수의 주소 = %p\n\n", &pointer);
+	 //배열의 메모리 공간은 프로그램이 실행되는 동안
+	 //변경할 수 없습니다.
 	
-	// *pointer = 99;
+	 int array[ ] = {1, 2, 3, 4, 5,};
 	
-	// printf("storage 변수의 값 = %d\n", storage);
+	//  int size = sizeof(array) / sizeof(array[0]);
 	
-	// printf("pointer 변수의 값 = %d\n\n", *pointer);
+	//  printf("%d\n\n", size);
 	
-	// int variable = 20;
+	//  for (int i = 0; i < size; i++)
+	//  {
+	//		printf("array[%d] = %d\n", i, array[i]);
+	//  }
 	
-	// pointer = &variable;
+	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
+	// 설정한 요소에 따라 배열의 크기가 결정됩니다
+
+	// printf("배열의 시작 주소 = %p\n", array);
 	
-	// printf("variable 변수의 주소 = %p\n", &variable);
+	int* pointer = array;
+	pointer = pointer + 1;
+	*pointer = 25;
 	
-	// printf("pointer 변수의 주소 = %p\n", &pointer);
-	
-	// *pointer = 100;
-	
-	// printf("variable 변수의 값 = %d\n", variable);
-	
-	// printf("pointer 변수의 값 = %d\n", *pointer);
-	
-	// printf("pointer 변수가 가리키는 값 = %d\n\n", *pointer);
+	printf("pointer의 값 = %d\n", *pointer); // array[1]만 출력
 
-	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
-	// 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의
-	// 시작 주소를 가리키게 됩니다.
+	for (int i = 0; i < 5; i++) // 배열 전체 출력
+	{
+		printf("array[%d] = %d\n", i, array[i]);
+	}
 
-	// printf("int 크기 = %u\n", sizeof(int));
-	
-	// printf("pointer 변수의크기 = %u\n", sizeof(pointer));
-
-	//포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수
-	// 있는 크기로 정해지며, 한 번에 처리할 수 있는 크기는
-	// 운영체제에 따라 크기가 결정됩니다.
-#pragma endregion
-
-#pragma region 숙제
-
-	// printf("간단 계산기\n\n");
-	// 
-	// double num1, num2, value;
-	// char operator;
-	// 
-	// printf("첫번째 숫자 = ");
-	// scanf_s("%lf", &num1);
-	// 
-	// printf("연산자 = ");
-	// scanf_s(" %c", &operator, 1);
-	// 
-	// printf("두번째 숫자 = ");
-	// scanf_s("%lf", &num2);
-	// 
-	// value = num1;
-	// 
-	// switch (operator)
-	// {
-	// case '+' : 
-	// 	value += num2;
-	// 		printf("%.2lf + %.2lf = %.2lf\n", num1, num2, value);
-	// 		break;
-	// 
-	// case '-' :
-	// 	value -= num2;
-	// 		printf("%.2lf - %.2lf = %.2lf", num1, num2, value);
-	// 		break;
-	// case '*':
-	// 	value *= num2;
-	// 	printf("%.2lf * %.2lf = %.2lf", num1, num2, value);
-	// 	break;
-	// 
-	// case '/':
-	// 	
-	// 	value /= num2;
-	// 	printf("%.2lf / %.2lf = %.2lf", num1, num2, value);
-	// 	break;
-	// 
-	// default:
-	// 	printf("오류\n");
-	// 	break;
-	// }
-	// return 0;
-
-#pragma endregion
-
-#pragma region 범용 포인터
-	// 자료형이 정해지지 않은 상태로 모든 자료형을
-	// 저장할 수 있는 포인터입니다.
-	
-	// char rank = 'A';
-	// int count = 10;
-	// float angle = 3.14f;
-	
-	// void* reference = NULL; // 포인터 초기화
-	
-	// reference = &rank;
-	
-	// 범용 포인터는 메모리 주소에 접근해서 값을
-	// 변경할 수 없습니다.
-	
-	// *(char *)reference = 'B';
-	
-	// printf("rank 변수의 값 = %c\n", rank);
-	// printf("reference 변수의 값 = %p\n\n", reference);
-	
-	// reference = &count;
-	
-	// *(int*)reference = 50;
-	
-	// printf("count 변수의 값 = %d\n", count);
-	// printf("reference 변수의 값 = %p\n\n", reference);
-	
-	// reference = &angle;
-	
-	// *(float*)reference = 45.5f;
-	// printf("angle 변수의 값 = %f\n", angle);
-	// printf("reference 변수의 값 = %p\n\n", reference);
-
-	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
-	// 가리키는 변수의 자료형으로 형 변환 해주어야 합니다.
-#pragma endregion
-
-#pragma region 상수 지시 포인터
-// 포인터가 가리키는 주소에 저장되어 있는 값을 변경할 수 없도록
-// 지정되어 있는 포인터 입니다.
-
-// int room1 = 10;
-// int room2 = 20;
-
-// const int * home = NULL;
-
-// home = &room1;
-
-// *home = 99; (x)
-
-// printf("room1의 주소 값 = %p\n", &room1);
-// printf("home의 값 = %p\n\n", home);
-
-// home = &room2;
-
-// printf("room2의 주소 값 = %p\n", &room2);
-// printf("home의 값 = %p\n", home);
-#pragma endregion
-
-#pragma region 포인터 함수
-// 포인터 변수가 가리키고 있는 주소 값을
-// 변경할 수 없는 포인터 입니다.
-
-int item1 = 99;
-int item2 = 555;
-
-int* const box = &item1;
-
-
-
-*box = 100;
-
-printf("item1의 주소값 = %d\n", item1);
-printf("box의 값 = %d\n", box);
-
-*box = item2;
-
-printf("item2의 주소값 = %d\n", item2);
-printf("box의 값 = %d\n", box);
+	// 배열은 연속적인 메모리 공간을 가지고 있으며, 배열의 이름은
+	// 배열의 시작 주소를 기리킵니다.
 #pragma endregion
 }
